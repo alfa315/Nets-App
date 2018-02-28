06fb7595bb9b861b3128b90d78a67665d3f7ab10
github username <github email address>
2018-02-28 15:11:24 -0500

    temp welcome page update

--- src/components/welcome/WelcomePage.js
+++ src/components/welcome/WelcomePage.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router'
+import { Button } from 'semantic-ui-react'
+import vid from '../../images/Nets.mp4'
 
-const WelcomePage = (props) => {
-  return (
-    <div className='bgimage extend'>
-      <div className='tint extend'>
-        <h1 className='welcomeHeader'>Welcome</h1>
+class WelcomePage extends Component {
+  state = {
+    redirect: false
+  }
+
+  handleOnClick = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  render() {
+    if(this.state.redirect) {
+      return <Redirect to='/home'/>
+    }
+    return (
+      <div>
+        <div className='extend'>
+          <Button onClick={this.handleOnClick} >Enter</Button>
+          <video
+          className="exten"
+          preload="auto"
+          autoPlay="true"
+          loop="loop"
+          muted="muted">
+            <source src={vid} type="video/mp4" />
+          </video>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default WelcomePage
