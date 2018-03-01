2ff6cf11974167aaf2d6330f3cc85e487135e934
github username <github email address>
2018-03-01 14:34:49 -0500

    added basic search box functionality

--- src/components/navbar/NavMenu.js
+++ src/components/navbar/NavMenu.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Menu, Segment, Input, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import logo from './logo.png'
+import logo from '../../images/logo.png'
 
 export default class NavMenu extends Component {
+  state = {
+    searchValue: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchValue: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    window.location.replace("http://localhost:3000/home")
+  }
+
+  handleClick = (event) => {
+    event.preventDefault()
+    window.location.replace("http://localhost:3000/home")
+  }
+
 
   render() {
     return (
       <Segment inverted>
         <Menu inverted secondary>
           <Link to='/' >
-            <img className='logo' src={logo} alt='Brooklyn Nets'/>
+            <img
+              className='logo'
+              src={logo}
+              alt='Brooklyn Nets'
+            />
           </Link>
+
           <Link to='/home' className="item">
             Home
           </Link>
           <Link to='/team' className="item">
             Current Roster
           </Link>
+
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <form onSubmit={this.handleSubmit}>
+                <Input
+                  onChange={this.handleChange}
+                  icon={ <Icon onClick={this.handleClick} name='search' link /> }
+                  placeholder='Search Players...'
+                />
+              </form>
+            </Menu.Item>
+          </Menu.Menu>
         </Menu>
       </Segment>
     )
   }
 }
+
+
+// I should move the search box into its own component, then link search enter and click to search results page, which will then lead back to player profile for each result searched for.
