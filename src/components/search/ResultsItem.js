7b6ed9822f8ab9e9c607eed4e4dd93d4de498775
github username <github email address>
2018-03-02 15:41:15 -0500

    fixed search error bug

--- src/components/search/ResultsItem.js
+++ src/components/search/ResultsItem.js
@@ -1,22 +1,29 @@
 import React from 'react'
+import MainLoader from '../loaders/MainLoader.js'
 import { List, Image } from 'semantic-ui-react'
 
 const ResultsItem = (props) => {
 
-  let lastName = props.results.lastName.replace(/[.']/g,'')
-  let firstName = props.results.firstName.replace(/[.']/g,'')
+  if(props.results.length === 0 || props.results === undefined) {
+    return (
+      <MainLoader />
+    )
+  } else {
+    let lastName = props.results.lastName.replace(/[.']/g,'')
+    let firstName = props.results.firstName.replace(/[.']/g,'')
 
-  let team = props.teams.find(o => o.teamId === props.results.teamId)
+    let team = props.teams.find(o => o.teamId === props.results.teamId)
 
-  return (
-    <List.Item className='searchItem'>
-      <Image circular size='small'  src={`https://nba-players.herokuapp.com/players/${lastName}/${firstName}`} />
-      <List.Content className='searchContent'>
-        <a href={`/team/${props.id}`}><List.Header as='a'>{props.results.firstName} {props.results.lastName}</List.Header></a>
-        <List.Description><b>{props.results.pos}: #{props.results.jersey} - {team.fullName} </b></List.Description>
-      </List.Content>
-    </List.Item>
-  )
+    return (
+      <List.Item className='searchItem'>
+        <Image circular size='small'  src={`https://nba-players.herokuapp.com/players/${lastName}/${firstName}`} />
+        <List.Content className='searchContent'>
+          <a href={`/team/${props.id}`}><List.Header as='a'>{props.results.firstName} {props.results.lastName}</List.Header></a>
+          <List.Description><b>{props.results.pos}: #{props.results.jersey} - {team.fullName} </b></List.Description>
+        </List.Content>
+      </List.Item>
+    )
+  }
 }
 
 export default ResultsItem
