c7f51b633a0552260bd3171b48120721718260ce
github username <github email address>
2018-03-04 17:39:17 -0500

    updated scoreboard to reflect final games and winner along with some style changes

--- src/components/home/GameCard.js
+++ src/components/home/GameCard.js
@@ -14,15 +14,20 @@ const GameCard = (props) => {
          <Card>
            <Card.Content>
              <Card.Description>
-               <p>{ev.vTeam.triCode} {ev.isGameActivated ? <p>{ev.vTeam.score}</p> : ev.statusNum === 3 ? <p>{ev.vTeam.score}</p> : <p>{"(" + ev.vTeam.win + "-" + ev.vTeam.loss + ")"}</p>}</p>
+               {ev.statusNum === 3 && ev.vTeam.score > ev.hTeam.score ? <p><b><i>{ev.vTeam.triCode}</i></b></p> : <p>{ev.vTeam.triCode}</p>}
+
+               {ev.isGameActivated ? <p>{ev.vTeam.score}</p> : ev.statusNum === 3 ? <p>{ev.vTeam.score}</p> : <p>{"(" + ev.vTeam.win + "-" + ev.vTeam.loss + ")"}</p>}
 
                <p>@</p>
 
-               <p>{ev.hTeam.triCode} {ev.isGameActivated ? <p>{ev.hTeam.score}</p> : ev.statusNum === 3 ? <p>{ev.hTeam.score}</p> : <p>{"(" + ev.hTeam.win + "-" + ev.hTeam.loss + ")"}</p>}</p>
+               {ev.statusNum === 3 && ev.hTeam.score > ev.vTeam.score ? <p><b><i>{ev.hTeam.triCode}</i></b></p> : <p>{ev.hTeam.triCode}</p>}
+
+               {ev.isGameActivated ? <p>{ev.hTeam.score}</p> : ev.statusNum === 3 ? <p>{ev.hTeam.score}</p> : <p>{"(" + ev.hTeam.win + "-" + ev.hTeam.loss + ")"}</p>}
              </Card.Description>
            </Card.Content>
+
            <Card.Content extra className='cardBtm'>
-            {ev.isGameActivated ? <p> {ev.clock} {ev.period.current + "Q"} </p> : ev.statusNum === 3 ? <p>FINAL</p> : <p>{ev.startTimeEastern}</p>}
+            {ev.isGameActivated ? <p> {ev.clock} {ev.period.current + "Q"} </p> : ev.statusNum === 3 ? <p><b>FINAL</b></p> : <p>{ev.startTimeEastern}</p>}
            </Card.Content>
          </Card>
         )
